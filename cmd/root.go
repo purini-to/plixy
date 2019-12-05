e479df2fdb10aec243c1e94cf93630dcf94f2ac7
hiroshi urainkyo <biz.hr.urainkyo@gmail.com>
2019-12-05 23:35:18 +0900

    #33 Add health endpoint

--- cmd/root.go
+++ cmd/root.go
@@ -3,12 +3,12 @@ package cmd
 import (
 	"context"
 
+	"github.com/purini-to/plixy/pkg/config"
+
 	"github.com/spf13/viper"
 
 	"github.com/spf13/cobra"
 )
-
-const version = "v0.0.0-dev"
 
 var debug bool
 
@@ -21,7 +21,7 @@ func NewRootCmd() *cobra.Command {
 		Short: "Plixy is an API Gateway",
 		Long: `
 This is a lightweight API Gateway.`,
-		Version: version,
+		Version: config.Version,
 	}
 
 	cmd.PersistentFlags().BoolVarP(&debug, "debug", "", false, "Launch in debug mode if there is a flag")
